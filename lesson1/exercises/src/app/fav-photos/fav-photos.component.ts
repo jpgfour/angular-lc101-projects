3c6151a6ca4ce0ed36f9131320d47c07ec60788f
Jeremy <jeremy.grise@gmail.com>
2020-03-29 16:36:20 -0500

    NOW adding all lesson 1 changes too

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
+  photosTitle = "Doin's & Transpirin's";
   image1 = 'https://www.launchcode.org/assets/icons/trophy-95e8cbe9bfda44123422302951deb1c92a237d39052669b8fbfafec00cb4f608.png';
-  image2 = '';
-  image3 = '';
+  image2 = 'https://media.graytvinc.com/images/690*517/coronavirus14.jpg';
+  image3 = 'https://completewellbeing.com/wp-content/uploads/2016/02/the-zen-of-work.jpg';
 
   constructor() { }
 
